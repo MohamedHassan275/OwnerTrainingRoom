bcf2169cf4703d9f71d4f93b8ab1f97176c697be
MohamedHassan275 <mohamed209059@gmail.com>
2019-04-05 20:13:02 +0200

    Initial commit

---
+++ app/src/main/java/com/mohmedhassan/ownertrainingroom/AddRooms/AddRoomsActivity.java
@@ -0,0 +1,37 @@
+package com.mohmedhassan.ownertrainingroom.AddRooms;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mohmedhassan.ownertrainingroom.AddTrainingRooms.AddTrainingRoomsActivity;
+import com.mohmedhassan.ownertrainingroom.R;
+
+public class AddRoomsActivity extends AppCompatActivity implements AddRoomsContract.view {
+
+    Button AddRooms;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rooms);
+
+        AddRooms = findViewById(R.id.add_rooms_AddRooms);
+
+        AddRooms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(AddRoomsActivity.this, AddTrainingRoomsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public void AddRooms() {
+
+    }
+}
